1aef3ea9f7c58e07d33b1c1d4fade2bd75f97c59
KIenVu41 <kienvutrung20@gmail.com>
2023-08-09 23:52:55 +0700

    add login/signup UI

---
+++ app/src/main/java/com/kv/connectify/FragmentReplacerActivity.kt
@@ -0,0 +1,11 @@
+package com.kv.connectify
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FragmentReplacerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_fragment_replacer)
+    }
+}
